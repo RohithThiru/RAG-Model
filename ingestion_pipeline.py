5b8b304894ee888cf1dda55318391d530c54c305
RohithThiru <rohithsharama6374@gmail.com>
2025-12-07 22:28:35 +0530

    Creating chunks and storing in vector DB

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -5,6 +5,8 @@ from langchain_chroma import Chroma
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 
 def load_documents(docs_path="docs"):
     """Load all text files from the docs directory"""
@@ -40,9 +42,62 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks...")
+    
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap
+    )
+    
+    chunks = text_splitter.split_documents(documents)
+    
+    if chunks:
+    
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n--- Chunk {i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-" * 50)
+        
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+    
+    return chunks
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings and storing in ChromaDB...")
+        
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    
+    # Create ChromaDB vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, 
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+    print("--- Finished creating vector store ---")
+    
+    print(f"Vector store created and saved to {persist_directory}")
+    return vectorstore
+
 
 def main():
-    load_documents(docs_path="docs")
+    persistent_directory = "db/chroma_db"
+
+    documents = load_documents(docs_path="docs")
+
+    chunks = split_documents(documents)
+
+    vectorstore = create_vector_store(chunks, persistent_directory)
+
+    return vectorstore
 
 
 if __name__ == "__main__":
